7c0b19037fe5904b7c5d5e18b09c38be04c4a07f
Tanuj <Tanuj@cmu.edu>
2025-05-02 07:25:58 -0700

    Update approvals.tsx
    - Add authentication component to the page

--- src/routes/approvals.tsx
+++ src/routes/approvals.tsx
@@ -1,6 +1,28 @@
 import { createFileRoute } from '@tanstack/react-router';
+import { Authenticated, Unauthenticated, AuthLoading } from "convex/react";
+import { SignInFormPassword } from "../CustomSignIn";
+import { LoadingIndicator } from "../components";
+import { AuthenticatedLayout } from "../components/layouts/AuthenticatedLayout";
 import { ApprovalWorkflow } from '../components/ApprovalWorkflow';
 
+function Approvals() {
+  return (
+    <>
+      <AuthLoading>
+        <LoadingIndicator />
+      </AuthLoading>
+      <Unauthenticated>
+        <SignInFormPassword />
+      </Unauthenticated>
+      <Authenticated>
+        <AuthenticatedLayout>
+          <ApprovalWorkflow />
+        </AuthenticatedLayout>
+      </Authenticated>
+    </>
+  );
+}
+
 export const Route = createFileRoute('/approvals')({
-  component: ApprovalWorkflow,
+  component: Approvals,
 }); 
